3b18dfd63fd852966b1186b302cd6c98bdb03a48
Chris Toshok <toshok@gmail.com>
2024-01-06 20:56:17 -0800

    more futzing

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/apollo.png",
     navbar: {
       title: "Domain/OS Archeology",
       logo: {
